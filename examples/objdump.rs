997706eba55290bb5653765fdc9221ef07cc3868
Dan Burkert <dan@danburkert.com>
2017-10-28 16:05:19 -0700

    Update to memmap 0.6
    `memmap` 0.6.0 introduces major API changes in anticipation of a 1.0
    release. See https://github.com/danburkert/memmap-rs/releases/tag/0.6.0
    for more information. CC danburkert/memmap-rs#33.

--- examples/objdump.rs
+++ examples/objdump.rs
@@ -23,14 +23,14 @@ fn main() {
                 return;
             }
         };
-        let file = match memmap::Mmap::open(&file, memmap::Protection::Read) {
+        let file = match unsafe { memmap::Mmap::map(&file) } {
             Ok(mmap) => mmap,
             Err(err) => {
                 println!("Failed to map file '{}': {}", file_path, err,);
                 return;
             }
         };
-        let file = match object::File::parse(unsafe { file.as_slice() }) {
+        let file = match object::File::parse(&*file) {
             Ok(file) => file,
             Err(err) => {
                 println!("Failed to parse file '{}': {}", file_path, err);
