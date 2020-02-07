839dd686ecdb1b464b0994ff926f91d530558ceb
Philip Craig <philipjcraig@gmail.com>
2020-02-07 16:18:49 +1000

    write/coff: fix writing TLS sections/symbols
    This still hasn't been tested with a linker though.

--- tests/round_trip/tls.rs
+++ tests/round_trip/tls.rs
@@ -8,6 +8,140 @@ use object::{
 use target_lexicon::{Architecture, BinaryFormat};
 
 #[test]
+fn coff_x86_64_tls() {
+    let mut object = write::Object::new(BinaryFormat::Coff, Architecture::X86_64);
+
+    let section = object.section_id(write::StandardSection::Tls);
+    let symbol = object.add_symbol(write::Symbol {
+        name: b"tls1".to_vec(),
+        value: 0,
+        size: 0,
+        kind: SymbolKind::Tls,
+        scope: SymbolScope::Linkage,
+        weak: false,
+        section: write::SymbolSection::Undefined,
+        flags: SymbolFlags::None,
+    });
+    object.add_symbol_data(symbol, section, &[1; 30], 4);
+
+    let bytes = object.write().unwrap();
+
+    //std::fs::write(&"tls.o", &bytes).unwrap();
+
+    let object = read::File::parse(&bytes).unwrap();
+    assert_eq!(object.format(), BinaryFormat::Coff);
+    assert_eq!(object.architecture(), Architecture::X86_64);
+
+    let mut sections = object.sections();
+
+    let section = sections.next().unwrap();
+    println!("{:?}", section);
+    let tls_index = section.index();
+    assert_eq!(section.name(), Some(".tls$"));
+    assert_eq!(section.kind(), SectionKind::Data);
+    assert_eq!(section.size(), 30);
+    assert_eq!(&section.data()[..], &[1; 30]);
+
+    let mut symbols = object.symbols();
+
+    let (_, symbol) = symbols.next().unwrap();
+    println!("{:?}", symbol);
+    assert_eq!(symbol.name(), Some("tls1"));
+    assert_eq!(symbol.kind(), SymbolKind::Data);
+    assert_eq!(symbol.section_index(), Some(tls_index));
+    assert_eq!(symbol.scope(), SymbolScope::Linkage);
+    assert_eq!(symbol.is_weak(), false);
+    assert_eq!(symbol.is_undefined(), false);
+}
+
+#[test]
+fn elf_x86_64_tls() {
+    let mut object = write::Object::new(BinaryFormat::Elf, Architecture::X86_64);
+
+    let section = object.section_id(write::StandardSection::Tls);
+    let symbol = object.add_symbol(write::Symbol {
+        name: b"tls1".to_vec(),
+        value: 0,
+        size: 0,
+        kind: SymbolKind::Tls,
+        scope: SymbolScope::Linkage,
+        weak: false,
+        section: write::SymbolSection::Undefined,
+        flags: SymbolFlags::None,
+    });
+    object.add_symbol_data(symbol, section, &[1; 30], 4);
+
+    let section = object.section_id(write::StandardSection::UninitializedTls);
+    let symbol = object.add_symbol(write::Symbol {
+        name: b"tls2".to_vec(),
+        value: 0,
+        size: 0,
+        kind: SymbolKind::Tls,
+        scope: SymbolScope::Linkage,
+        weak: false,
+        section: write::SymbolSection::Undefined,
+        flags: SymbolFlags::None,
+    });
+    object.add_symbol_bss(symbol, section, 31, 4);
+
+    let bytes = object.write().unwrap();
+
+    //std::fs::write(&"tls.o", &bytes).unwrap();
+
+    let object = read::File::parse(&bytes).unwrap();
+    assert_eq!(object.format(), BinaryFormat::Elf);
+    assert_eq!(object.architecture(), Architecture::X86_64);
+
+    let mut sections = object.sections();
+
+    let section = sections.next().unwrap();
+    println!("{:?}", section);
+    assert_eq!(section.name(), Some(""));
+
+    let section = sections.next().unwrap();
+    println!("{:?}", section);
+    let tdata_index = section.index();
+    assert_eq!(section.name(), Some(".tdata"));
+    assert_eq!(section.kind(), SectionKind::Tls);
+    assert_eq!(section.size(), 30);
+    assert_eq!(&section.data()[..], &[1; 30]);
+
+    let section = sections.next().unwrap();
+    println!("{:?}", section);
+    let tbss_index = section.index();
+    assert_eq!(section.name(), Some(".tbss"));
+    assert_eq!(section.kind(), SectionKind::UninitializedTls);
+    assert_eq!(section.size(), 31);
+    assert_eq!(&section.data()[..], &[]);
+
+    let mut symbols = object.symbols();
+
+    let (_, symbol) = symbols.next().unwrap();
+    println!("{:?}", symbol);
+    assert_eq!(symbol.name(), Some(""));
+
+    let (_, symbol) = symbols.next().unwrap();
+    println!("{:?}", symbol);
+    assert_eq!(symbol.name(), Some("tls1"));
+    assert_eq!(symbol.kind(), SymbolKind::Tls);
+    assert_eq!(symbol.section_index(), Some(tdata_index));
+    assert_eq!(symbol.scope(), SymbolScope::Linkage);
+    assert_eq!(symbol.is_weak(), false);
+    assert_eq!(symbol.is_undefined(), false);
+    assert_eq!(symbol.size(), 30);
+
+    let (_, symbol) = symbols.next().unwrap();
+    println!("{:?}", symbol);
+    assert_eq!(symbol.name(), Some("tls2"));
+    assert_eq!(symbol.kind(), SymbolKind::Tls);
+    assert_eq!(symbol.section_index(), Some(tbss_index));
+    assert_eq!(symbol.scope(), SymbolScope::Linkage);
+    assert_eq!(symbol.is_weak(), false);
+    assert_eq!(symbol.is_undefined(), false);
+    assert_eq!(symbol.size(), 31);
+}
+
+#[test]
 fn macho_x86_64_tls() {
     let mut object = write::Object::new(BinaryFormat::Macho, Architecture::X86_64);
 
