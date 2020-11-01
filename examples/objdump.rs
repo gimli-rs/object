dffa9d489606d76cb2ad6dd602eb089cb97c459a
Philip Craig <philipjcraig@gmail.com>
2020-11-01 19:06:40 +1000

    read: implement Mach-O fat binary parsing

--- examples/objdump.rs
+++ examples/objdump.rs
@@ -1,3 +1,5 @@
+use object::read::archive::ArchiveFile;
+use object::read::macho::{FatArch, FatHeader};
 use object::{Object, ObjectComdat, ObjectSection, ObjectSymbol};
 use std::{env, fs, process};
 
@@ -18,73 +20,113 @@ fn main() {
             Ok(file) => file,
             Err(err) => {
                 println!("Failed to open file '{}': {}", file_path, err,);
-                return;
+                continue;
             }
         };
         let file = match unsafe { memmap::Mmap::map(&file) } {
             Ok(mmap) => mmap,
             Err(err) => {
                 println!("Failed to map file '{}': {}", file_path, err,);
-                return;
-            }
-        };
-        let file = match object::File::parse(&*file) {
-            Ok(file) => file,
-            Err(err) => {
-                println!("Failed to parse file '{}': {}", file_path, err);
-                return;
+                continue;
             }
         };
 
-        match file.mach_uuid() {
-            Ok(Some(uuid)) => println!("Mach UUID: {:x?}", uuid),
-            Ok(None) => {}
-            Err(e) => println!("Failed to parse Mach UUID: {}", e),
-        }
-        match file.build_id() {
-            Ok(Some(build_id)) => println!("Build ID: {:x?}", build_id),
-            Ok(None) => {}
-            Err(e) => println!("Failed to parse build ID: {}", e),
-        }
-        match file.gnu_debuglink() {
-            Ok(Some((filename, crc))) => println!(
-                "GNU debug link: {} CRC: {:08x}",
-                String::from_utf8_lossy(filename),
-                crc,
-            ),
-            Ok(None) => {}
-            Err(e) => println!("Failed to parse GNU debug link: {}", e),
-        }
-
-        for segment in file.segments() {
-            println!("{:?}", segment);
-        }
-
-        for section in file.sections() {
-            println!("{}: {:?}", section.index().0, section);
-        }
-
-        for comdat in file.comdats() {
-            print!("{:?} Sections:", comdat);
-            for section in comdat.sections() {
-                print!(" {}", section.0);
-            }
-            println!();
-        }
-
-        for symbol in file.symbols() {
-            println!("{}: {:?}", symbol.index().0, symbol);
-        }
-
-        for section in file.sections() {
-            if section.relocations().next().is_some() {
-                println!(
-                    "\n{} relocations",
-                    section.name().unwrap_or("<invalid name>")
-                );
-                for relocation in section.relocations() {
-                    println!("{:?}", relocation);
+        if let Ok(archive) = ArchiveFile::parse(&*file) {
+            println!("Format: Archive (kind: {:?})", archive.kind());
+            for member in archive.members() {
+                if let Ok(member) = member {
+                    println!();
+                    println!("{}:", String::from_utf8_lossy(member.name()));
+                    dump_object(member.data());
                 }
+            }
+        } else if let Ok(arches) = FatHeader::parse_arch32(&*file) {
+            println!("Format: Mach-O Fat 32");
+            for arch in arches {
+                println!();
+                println!("Fat Arch: {:?}", arch.architecture());
+                if let Ok(data) = arch.data(&*file) {
+                    dump_object(data);
+                }
+            }
+        } else if let Ok(arches) = FatHeader::parse_arch64(&*file) {
+            println!("Format: Mach-O Fat 64");
+            for arch in arches {
+                println!();
+                println!("Fat Arch: {:?}", arch.architecture());
+                if let Ok(data) = arch.data(&*file) {
+                    dump_object(data);
+                }
+            }
+        } else {
+            dump_object(&*file);
+        }
+    }
+}
+
+fn dump_object(data: &[u8]) {
+    let file = match object::File::parse(data) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Failed to parse file: {}", err);
+            return;
+        }
+    };
+    println!(
+        "Format: {:?} {:?}-endian {}-bit",
+        file.format(),
+        file.endianness(),
+        if file.is_64() { "64" } else { "32" }
+    );
+    println!("Architecture: {:?}", file.architecture());
+    match file.mach_uuid() {
+        Ok(Some(uuid)) => println!("Mach UUID: {:x?}", uuid),
+        Ok(None) => {}
+        Err(e) => println!("Failed to parse Mach UUID: {}", e),
+    }
+    match file.build_id() {
+        Ok(Some(build_id)) => println!("Build ID: {:x?}", build_id),
+        Ok(None) => {}
+        Err(e) => println!("Failed to parse build ID: {}", e),
+    }
+    match file.gnu_debuglink() {
+        Ok(Some((filename, crc))) => println!(
+            "GNU debug link: {} CRC: {:08x}",
+            String::from_utf8_lossy(filename),
+            crc,
+        ),
+        Ok(None) => {}
+        Err(e) => println!("Failed to parse GNU debug link: {}", e),
+    }
+
+    for segment in file.segments() {
+        println!("{:?}", segment);
+    }
+
+    for section in file.sections() {
+        println!("{}: {:?}", section.index().0, section);
+    }
+
+    for comdat in file.comdats() {
+        print!("{:?} Sections:", comdat);
+        for section in comdat.sections() {
+            print!(" {}", section.0);
+        }
+        println!();
+    }
+
+    for symbol in file.symbols() {
+        println!("{}: {:?}", symbol.index().0, symbol);
+    }
+
+    for section in file.sections() {
+        if section.relocations().next().is_some() {
+            println!(
+                "\n{} relocations",
+                section.name().unwrap_or("<invalid name>")
+            );
+            for relocation in section.relocations() {
+                println!("{:?}", relocation);
             }
         }
     }
