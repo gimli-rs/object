87f41379cfe313a0b50255e61ed10757cdfd7619
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2021-11-18 23:07:54 +0100

    read/coff tests: Update path to .o file.

--- tests/coff.rs
+++ tests/coff.rs
@@ -5,15 +5,15 @@ use std::path::PathBuf;
 #[cfg(feature = "coff")]
 #[test]
 fn coff_extended_relocations() {
-    let path_to_obj: PathBuf = ["testfiles", "coff", "relocs_overflow.obj"]
+    let path_to_obj: PathBuf = ["testfiles", "coff", "relocs_overflow.o"]
         .iter()
         .collect();
-    let contents = fs::read(&path_to_obj).expect("Could not read relocs_overflow.obj");
+    let contents = fs::read(&path_to_obj).expect("Could not read relocs_overflow.o");
     let file =
-        read::coff::CoffFile::parse(&contents[..]).expect("Could not parse relocs_overflow.obj");
+        read::coff::CoffFile::parse(&contents[..]).expect("Could not parse relocs_overflow.o");
     let code_section = file
         .section_by_name(".text")
-        .expect("Could not find .text section in relocs_overflow.obj");
+        .expect("Could not find .text section in relocs_overflow.o");
     match code_section.flags() {
         object::SectionFlags::Coff { characteristics } => {
             assert!(characteristics & pe::IMAGE_SCN_LNK_NRELOC_OVFL != 0)
