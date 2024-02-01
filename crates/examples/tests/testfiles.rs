789c859abb7011bb110085f597c30b32c02bdd6b
Philip Craig <philipjcraig@gmail.com>
2024-02-01 15:10:45 +1000

    Allow generation of all output testfiles (#628)
    Some of the output testfiles are only the partial output.
    Extend the OBJECT_TESTFILES_UPDATE support to handle these too.
    
    Also, reverse the derivation of filenames: now the input filename
    is derived from the output filename. This allows us to detect
    output files that don't have a corresponding input file.

--- crates/examples/tests/testfiles.rs
+++ crates/examples/tests/testfiles.rs
@@ -4,8 +4,7 @@
 use object_examples::objcopy;
 use object_examples::{objdump, readobj};
 use std::ffi::OsStr;
-use std::io::Write;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::{env, fs};
 
 const DISABLED_TEST_DIRS: &[&'static str] = &[
@@ -15,7 +14,7 @@ const DISABLED_TEST_DIRS: &[&'static str] = &[
     "xcoff",
 ];
 
-fn test_dir_filter(path: &PathBuf) -> bool {
+fn test_dir_enabled(path: &Path) -> bool {
     match path.file_name().and_then(OsStr::to_str) {
         Some(dir) => !DISABLED_TEST_DIRS.contains(&dir),
         None => true,
@@ -26,105 +25,158 @@ fn test_dir_filter(path: &PathBuf) -> bool {
 fn testfiles() {
     let update = env::var_os("OBJECT_TESTFILES_UPDATE").is_some();
 
-    // Move from crates/examples to the workspace root.
-    env::set_current_dir("../..").unwrap();
+    let in_testfiles = PathBuf::from("../..");
+    let out_testfiles = PathBuf::from("testfiles");
 
     let mut fail = false;
-    for dir in glob::glob("testfiles/*")
-        .unwrap()
-        .filter_map(Result::ok)
-        .filter(test_dir_filter)
-    {
-        let dir = dir.to_str().unwrap();
-        for path in glob::glob(&format!("{}/*", dir))
-            .unwrap()
-            .filter_map(Result::ok)
-        {
-            let path = path.to_str().unwrap();
-            if glob::glob(&format!("crates/examples/{}.*", path))
-                .unwrap()
-                .find_map(Result::ok)
-                .is_none()
-            {
+    let mut dirs = vec![out_testfiles];
+    while let Some(dir) = dirs.pop() {
+        for entry in dir.read_dir().unwrap() {
+            let entry = entry.unwrap();
+            let out_path = entry.path();
+            let file_type = entry.file_type().unwrap();
+            if file_type.is_dir() {
+                if test_dir_enabled(&out_path) {
+                    dirs.push(out_path);
+                }
                 continue;
             }
-
-            println!("File {}", path);
-            let data = fs::read(&path).unwrap();
-            fail |= testfile(update, path, &data, "objdump", |mut out, mut err, data| {
-                objdump::print(&mut out, &mut err, data, &[], vec![]).unwrap()
-            });
-            fail |= testfile(update, path, &data, "readobj", |out, err, data| {
-                readobj::print(out, err, data, &readobj::PrintOptions::all())
-            });
-
-            #[cfg(feature = "write")]
-            {
-                fail |= testfile(update, path, &data, "objcopy", |out, err, in_data| {
-                    let out_data = objcopy::copy(in_data);
-                    readobj::print(out, err, &out_data, &readobj::PrintOptions::all())
-                });
+            if !file_type.is_file() {
+                continue;
             }
-            println!();
+            let extension = out_path.extension().unwrap().to_str().unwrap();
+
+            let mut in_path = in_testfiles.join(&out_path);
+            in_path.set_extension("");
+
+            if extension == "err" {
+                continue;
+            }
+            // TODO: use err_path.as_mut_os_string().push(".err") when MSRV is >= 1.70.
+            let mut err_path = out_path.clone().into_os_string();
+            err_path.push(".err");
+            let err_path = PathBuf::from(err_path);
+            let err_path = if err_path.is_file() {
+                Some(&*err_path)
+            } else {
+                None
+            };
+
+            if extension == "objdump" {
+                fail |= testfile(update, &in_path, &out_path, err_path, |out, err, data| {
+                    objdump::print(out, err, data, &[], vec![]).unwrap();
+                });
+            } else if extension == "objdump-shndx" {
+                // Special case for the large symtab shndx test.
+                fail |= testfile(update, &in_path, &out_path, err_path, |out, err, data| {
+                    objdump::print(out, err, data, &[], vec![]).unwrap();
+                    *out = filter_lines(&*out, |line| {
+                        line.starts_with("6553") && line[5..].starts_with(": Symbol {")
+                    });
+                });
+            } else if extension == "objdump-comdat" {
+                fail |= testfile(update, &in_path, &out_path, err_path, |out, err, data| {
+                    objdump::print(out, err, data, &[], vec![]).unwrap();
+                    *out = filter_lines(&*out, |line| line.starts_with("Comdat "));
+                });
+            } else if extension.starts_with("readobj") {
+                let options = match extension {
+                    "readobj" => readobj::PrintOptions {
+                        pe_base_relocs: false, // Too many
+                        ..readobj::PrintOptions::all()
+                    },
+                    "readobj-section" => readobj::PrintOptions {
+                        sections: true,
+                        ..readobj::PrintOptions::none()
+                    },
+                    "readobj-elf-note" => readobj::PrintOptions {
+                        elf_notes: true,
+                        ..readobj::PrintOptions::none()
+                    },
+                    "readobj-pe-base-reloc" => readobj::PrintOptions {
+                        pe_base_relocs: true,
+                        ..readobj::PrintOptions::none()
+                    },
+                    "readobj-pe-resource" => readobj::PrintOptions {
+                        pe_resources: true,
+                        ..readobj::PrintOptions::none()
+                    },
+                    _ => {
+                        println!("Unknown test {}", out_path.display());
+                        fail = true;
+                        continue;
+                    }
+                };
+                fail |= testfile(update, &in_path, &out_path, err_path, |out, err, data| {
+                    readobj::print(out, err, data, &options);
+                });
+            } else if extension == "objcopy" {
+                #[cfg(feature = "write")]
+                {
+                    fail |= testfile(
+                        update,
+                        &in_path,
+                        &out_path,
+                        err_path,
+                        |out, err, in_data| {
+                            let copy_data = objcopy::copy(in_data);
+                            readobj::print(out, err, &copy_data, &readobj::PrintOptions::all());
+                        },
+                    );
+                }
+            } else {
+                println!("Unknown test {}", out_path.display());
+                fail = true;
+            }
         }
     }
-    assert!(!fail);
+    if fail {
+        panic!("Tests failed; rerun with OBJECT_TESTFILES_UPDATE=1 to update tests");
+    }
 }
 
-fn testfile<F>(update: bool, path: &str, data: &[u8], ext: &str, f: F) -> bool
+fn testfile<F>(update: bool, in_path: &Path, out_path: &Path, err_path: Option<&Path>, f: F) -> bool
 where
-    F: FnOnce(&mut dyn Write, &mut dyn Write, &[u8]),
+    F: FnOnce(&mut Vec<u8>, &mut Vec<u8>, &[u8]),
 {
-    if glob::glob(&format!("crates/examples/{}.{}*", path, ext))
-        .unwrap()
-        .find_map(Result::ok)
-        .is_none()
-    {
-        return false;
-    }
+    println!("Test {}", out_path.display());
+    let source = match fs::read(in_path) {
+        Ok(source) => source,
+        Err(err) => {
+            println!("FAIL {}", err);
+            return true;
+        }
+    };
 
     // TODO: print diffs for mismatches
     let mut fail = false;
-    let mut out = Vec::new();
-    let mut err = Vec::new();
-    f(&mut out, &mut err, data);
+    let mut out_data = Vec::new();
+    let mut err_data = Vec::new();
+    f(&mut out_data, &mut err_data, &source);
 
     // Check exact match of output.
-    let out_path = &format!("crates/examples/{}.{}", path, ext);
-    if let Ok(expect_out) = fs::read(out_path) {
-        println!("Test {}", out_path);
-        if update {
-            fs::write(out_path, &out).unwrap();
-        } else if out != expect_out {
+    if update {
+        fs::write(out_path, &out_data).unwrap();
+    } else {
+        let expect_out_data = fs::read(out_path).unwrap();
+        if out_data != expect_out_data {
             println!("FAIL mismatch");
-            fail = true;
-        }
-    }
-
-    // Check partial match of output.
-    for out_path in glob::glob(&format!("crates/examples/{}.{}.*", path, ext))
-        .unwrap()
-        .filter_map(Result::ok)
-    {
-        println!("Test {}", out_path.to_str().unwrap());
-        let expect_out = fs::read(&out_path).unwrap();
-        if !find_subslice(&out, &expect_out) {
-            println!("FAIL not found");
             fail = true;
         }
     }
 
     // Check exact match of errors.
-    let err_path = &format!("{}.err", out_path);
-    if let Ok(expect_err) = fs::read(err_path) {
-        println!("Test {}", err_path);
+    if let Some(err_path) = err_path {
         if update {
-            fs::write(err_path, &err).unwrap();
-        } else if err != expect_err {
-            println!("FAIL mismatch");
-            fail = true;
+            fs::write(err_path, &err_data).unwrap();
+        } else {
+            let expect_err_data = fs::read(err_path).unwrap();
+            if err_data != expect_err_data {
+                println!("FAIL mismatch");
+                fail = true;
+            }
         }
-    } else if !err.is_empty() {
+    } else if !err_data.is_empty() {
         println!("FAIL unexpected stderr");
         fail = true;
     }
@@ -132,8 +184,16 @@ where
     fail
 }
 
-fn find_subslice(haystack: &[u8], needle: &[u8]) -> bool {
-    haystack
-        .windows(needle.len())
-        .any(|window| window == needle)
+fn filter_lines<F>(data: &[u8], mut f: F) -> Vec<u8>
+where
+    F: FnMut(&str) -> bool,
+{
+    let mut result = Vec::new();
+    for line in data.split(|&b| b == b'\n') {
+        if f(std::str::from_utf8(line).unwrap()) {
+            result.extend_from_slice(line);
+            result.push(b'\n');
+        }
+    }
+    result
 }
