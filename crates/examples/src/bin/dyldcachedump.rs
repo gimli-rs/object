791adb0a1e41b591db085459e9128bc529b4a695
Markus Stange <markus.stange@gmail.com>
2024-03-11 01:01:56 -0400

    Correctly handle dyld caches on macOS 13 and above (#642)
    This allows successful parsing of dyld caches on
    macOS 13 and above on Intel Macs.
    
    The main dyld cache file on macOS contains an array of
    subcache info structs, each of which specifies the UUID
    (and some other information) of each subcache.
    `DyldCache::parse` checks that the subcache UUIDs match
    these expected UUIDs.
    
    In macOS 13, the format of the subcache info struct
    changed: it gained an additional field after the UUID
    field. This means that as soon as you had more than
    one subcache, our UUID check would fail, because the
    second subcache UUID would be read from the wrong offset.
    
    I didn't notice this on my Apple Silicon Mac, because
    the arm64e dyld cache only has one subcache:
    `dyld_shared_cache_arm64e.01`.
    But on Intel Macs, there are currently four subcaches:
    `dyld_shared_cache_x86_64.01`, `.02`, `.03`, and `.04`.
    
    In practice this means that my software hasn't been able to
    symbolicate macOS system libraries on Intel Macs since
    the release of macOS 13.
    
    This commit adds the new struct definition and makes
    the UUID check work correctly.
    
    This is a breaking change to the public API. I added
    a `DyldSubCacheSlice` enum, but I'm not particularly
    fond of it.
    
    dyldcachedump was working correctly on macOS 13+ because it was trying
    the "leading zero" suffix format as well as the "no leading zero" suffix
    format. This commit changes it to read the suffix from the main cache
    header.
    
    objdump was not able to parse dyld shared cache files on macOS 13+ because
    it was only using the "no leading zero" suffix format, and thus not finding
    the subcaches.

--- crates/examples/src/bin/dyldcachedump.rs
+++ crates/examples/src/bin/dyldcachedump.rs
@@ -1,4 +1,5 @@
-use object::read::macho::DyldCache;
+use object::macho::DyldCacheHeader;
+use object::read::macho::{DyldCache, DyldSubCacheSlice};
 use object::Endianness;
 use std::{env, fs, process};
 
@@ -22,7 +23,6 @@ fn main() {
                 continue;
             }
         };
-        let subcache_files = open_subcaches_if_exist(&file_path);
         let file = match unsafe { memmap2::Mmap::map(&file) } {
             Ok(mmap) => mmap,
             Err(err) => {
@@ -30,13 +30,27 @@ fn main() {
                 continue;
             }
         };
+
+        let subcaches_info = match get_subcache_info(&file) {
+            Ok(subcaches_info) => subcaches_info,
+            Err(err) => {
+                println!(
+                    "Failed to parse Dyld shared cache file '{}': {}",
+                    file_path, err,
+                );
+                continue;
+            }
+        };
+        let subcache_files = subcaches_info
+            .map(|info| open_subcaches(&file_path, info))
+            .unwrap_or_default();
         let subcache_files: Option<Vec<_>> = subcache_files
             .into_iter()
             .map(
                 |subcache_file| match unsafe { memmap2::Mmap::map(&subcache_file) } {
                     Ok(mmap) => Some(mmap),
                     Err(err) => {
-                        eprintln!("Failed to map file '{}': {}", file_path, err);
+                        println!("Failed to map file '{}': {}", file_path, err);
                         None
                     }
                 },
@@ -69,27 +83,50 @@ fn main() {
     }
 }
 
+/// Gets the slice of subcache info structs from the header of the main cache.
+fn get_subcache_info(
+    main_cache_data: &[u8],
+) -> object::read::Result<Option<DyldSubCacheSlice<'_, Endianness>>> {
+    let header = DyldCacheHeader::<Endianness>::parse(main_cache_data)?;
+    let (_arch, endian) = header.parse_magic()?;
+    let subcaches_info = header.subcaches(endian, main_cache_data)?;
+    Ok(subcaches_info)
+}
+
 // If the file is a dyld shared cache, and we're on macOS 12 or later,
 // then there will be one or more "subcache" files next to this file,
 // with the names filename.1, filename.2, ..., filename.symbols
-// or filename.01, filename.02 on macOS 13
-fn open_subcaches_if_exist(path: &str) -> Vec<fs::File> {
+// or filename.01, filename.02, ..., filename.symbols on macOS 13
+fn open_subcaches(path: &str, subcaches_info: DyldSubCacheSlice<Endianness>) -> Vec<fs::File> {
+    let subcache_suffixes: Vec<String> = match subcaches_info {
+        DyldSubCacheSlice::V1(subcaches) => {
+            // macOS 12: Subcaches have the file suffixes .1, .2, .3 etc.
+            (1..subcaches.len() + 1).map(|i| format!(".{i}")).collect()
+        }
+        DyldSubCacheSlice::V2(subcaches) => {
+            // macOS 13+: The subcache file suffix is written down in the header of the main cache.
+            subcaches
+                .iter()
+                .map(|s| {
+                    // The suffix is a nul-terminated string in a fixed-size byte array.
+                    let suffix = s.file_suffix;
+                    let len = suffix.iter().position(|&c| c == 0).unwrap_or(suffix.len());
+                    String::from_utf8_lossy(&suffix[..len]).to_string()
+                })
+                .collect()
+        }
+        _ => panic!(
+            "If this case is hit, it means that someone added a variant to the (non-exhaustive) \
+            DyldSubCacheSlice enum and forgot to update this example"
+        ),
+    };
     let mut files = Vec::new();
-    for i in 1.. {
-        let subcache_path = format!("{}.{}", path, i);
+    for suffix in subcache_suffixes {
+        let subcache_path = format!("{path}{suffix}");
         match fs::File::open(subcache_path) {
             Ok(subcache_file) => files.push(subcache_file),
             Err(_) => break,
         };
-    }
-    if files.is_empty() {
-        for i in 1.. {
-            let subcache_path = format!("{}.{:02}", path, i);
-            match fs::File::open(subcache_path) {
-                Ok(subcache_file) => files.push(subcache_file),
-                Err(_) => break,
-            };
-        }
     }
     let symbols_subcache_path = format!("{}.symbols", path);
     if let Ok(subcache_file) = fs::File::open(symbols_subcache_path) {
