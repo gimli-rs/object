4eb24311d9d8bcc4a705929f320466602daa37d3
OndrikB <ondricek.baco@gmail.com>
2024-08-10 21:16:33 +0200

    write/coff: Set checksum for BSS section symbols

--- src/write/coff/object.rs
+++ src/write/coff/object.rs
@@ -654,7 +654,12 @@ impl<'a> Object<'a> {
                         length: section.size as u32,
                         number_of_relocations: section.relocations.len() as u32,
                         number_of_linenumbers: 0,
-                        check_sum: checksum(section.data()),
+                        check_sum: if section.is_bss() { 
+                            0 
+                        } 
+                        else {
+                            checksum(section.data())
+                        },
                         number: section_offsets[section_index].associative_section,
                         selection: section_offsets[section_index].selection,
                     });
