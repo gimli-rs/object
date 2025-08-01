17b94ac5115dd8b8f758df50fdad50f99d6fb51a
Philip Craig <philipjcraig@gmail.com>
2025-08-01 15:57:37 +1000

    Fix clippy warnings
    Allow clippy::uninlined_format_args for now since fixing it is a lot of noise.

--- src/lib.rs
+++ src/lib.rs
@@ -46,21 +46,6 @@
 #![deny(missing_debug_implementations)]
 #![no_std]
 #![warn(rust_2018_idioms)]
-// Style.
-#![allow(clippy::collapsible_else_if)]
-#![allow(clippy::collapsible_if)]
-#![allow(clippy::collapsible_match)]
-#![allow(clippy::comparison_chain)]
-#![allow(clippy::field_reassign_with_default)]
-#![allow(clippy::manual_flatten)]
-#![allow(clippy::match_like_matches_macro)]
-#![allow(clippy::needless_lifetimes)]
-#![allow(clippy::single_match)]
-#![allow(clippy::type_complexity)]
-// Occurs due to fallible iteration.
-#![allow(clippy::should_implement_trait)]
-// Unit errors are converted to other types by callers.
-#![allow(clippy::result_unit_err)]
 
 #[cfg(feature = "cargo-all")]
 compile_error!("'--all-features' is not supported; use '--features all' instead");
