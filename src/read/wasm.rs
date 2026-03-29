dba544f333b4892ff899ddca5061cd58928a8611
Philip Craig <philipjcraig@gmail.com>
2026-03-29 13:42:22 +1000

    Update wasmparser dependency

--- src/read/wasm.rs
+++ src/read/wasm.rs
@@ -107,7 +107,7 @@ impl<'data, R: ReadRef<'data>> WasmFile<'data, R> {
         let mut entry_func_id = None;
         let mut code_range_start = 0;
         let mut code_ranges = Vec::new();
-        let mut imports = None;
+        let mut imports_section = None;
         let mut exports = None;
         let mut names = None;
         let mut symbols = None;
@@ -128,7 +128,7 @@ impl<'data, R: ReadRef<'data>> WasmFile<'data, R> {
                 }
                 wp::Payload::ImportSection(section) => {
                     file.add_section(SectionId::Import, section.range(), "");
-                    imports = Some(section);
+                    imports_section = Some(section);
                 }
                 wp::Payload::FunctionSection(section) => {
                     file.add_section(SectionId::Function, section.range(), "");
@@ -229,57 +229,75 @@ impl<'data, R: ReadRef<'data>> WasmFile<'data, R> {
 
         let mut import_func_names = Vec::new();
         let mut import_global_names = Vec::new();
-        if let Some(imports) = imports {
+        if let Some(imports_section) = imports_section {
             let mut last_module_name = None;
 
-            for import in imports {
-                let import = import.read_error("Couldn't read an import item")?;
-                let kind = match import.ty {
-                    wp::TypeRef::Func(_) | wp::TypeRef::FuncExact(_) => {
-                        import_func_names.push(import.name);
-                        SymbolKind::Text
-                    }
-                    wp::TypeRef::Memory(memory) => {
-                        file.has_memory64 |= memory.memory64;
-                        SymbolKind::Data
-                    }
-                    wp::TypeRef::Global(_) => {
-                        import_global_names.push(import.name);
-                        SymbolKind::Data
-                    }
-                    wp::TypeRef::Table(_) => SymbolKind::Data,
-                    wp::TypeRef::Tag(_) => SymbolKind::Unknown,
-                };
+            for imports in imports_section {
+                let imports = imports.read_error("Couldn't read an imports item")?;
+                let add_import = &mut |module, ty, name| {
+                    let kind = match ty {
+                        wp::TypeRef::Func(_) | wp::TypeRef::FuncExact(_) => {
+                            import_func_names.push(name);
+                            SymbolKind::Text
+                        }
+                        wp::TypeRef::Memory(memory) => {
+                            file.has_memory64 |= memory.memory64;
+                            SymbolKind::Data
+                        }
+                        wp::TypeRef::Global(_) => {
+                            import_global_names.push(name);
+                            SymbolKind::Data
+                        }
+                        wp::TypeRef::Table(_) => SymbolKind::Data,
+                        wp::TypeRef::Tag(_) => SymbolKind::Unknown,
+                    };
 
-                if symbols.is_some() {
-                    // We have a symbol table, so we don't need to add symbols for imports.
-                    // TODO: never add symbols for imports. Return them via Object::imports instead.
-                    continue;
-                }
+                    if symbols.is_some() {
+                        // We have a symbol table, so we don't need to add symbols for imports.
+                        // TODO: never add symbols for imports. Return them via Object::imports instead.
+                        return;
+                    }
 
-                let module_name = import.module;
-                if last_module_name != Some(module_name) {
+                    if last_module_name != Some(module) {
+                        file.symbols.push(WasmSymbolInternal {
+                            name: module,
+                            address: 0,
+                            size: 0,
+                            kind: SymbolKind::File,
+                            section: SymbolSection::None,
+                            scope: SymbolScope::Dynamic,
+                            weak: false,
+                        });
+                        last_module_name = Some(module);
+                    }
+
                     file.symbols.push(WasmSymbolInternal {
-                        name: module_name,
+                        name,
                         address: 0,
                         size: 0,
-                        kind: SymbolKind::File,
-                        section: SymbolSection::None,
+                        kind,
+                        section: SymbolSection::Undefined,
                         scope: SymbolScope::Dynamic,
                         weak: false,
                     });
-                    last_module_name = Some(module_name);
+                };
+                match imports {
+                    wp::Imports::Single(_, import) => {
+                        add_import(import.module, import.ty, import.name);
+                    }
+                    wp::Imports::Compact1 { module, items } => {
+                        for item in items {
+                            let item = item.read_error("Couldn't read an imports item")?;
+                            add_import(module, item.ty, item.name);
+                        }
+                    }
+                    wp::Imports::Compact2 { module, ty, names } => {
+                        for name in names {
+                            let name = name.read_error("Couldn't read an imports name")?;
+                            add_import(module, ty, name);
+                        }
+                    }
                 }
-
-                file.symbols.push(WasmSymbolInternal {
-                    name: import.name,
-                    address: 0,
-                    size: 0,
-                    kind,
-                    section: SymbolSection::Undefined,
-                    scope: SymbolScope::Dynamic,
-                    weak: false,
-                });
             }
         }
 
